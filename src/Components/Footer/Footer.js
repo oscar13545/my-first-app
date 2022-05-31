e1c4bc759415c40aa6762b29be2390390de34898
oscar13545 <Oscar.herediam10@gmail.com>
2022-05-31 12:44:40 -0600

    Prueba Final

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Col, Row, Card} from 'react-bootstrap';
+import Logo from  './Logo.png'; 
+import './Footer.css';
 
 const Footer = (props) => {
 
@@ -14,16 +16,27 @@ const Footer = (props) => {
           <Container>
 
 <Row>
-<Col xs={6}>  <h5 >La escondida</h5>
-                <p>EL MEJOR LUGAR PARA REALIZAR TU EVENTO</p></Col>
+<Col xs={6}>  <img
+        src={Logo}
+        width="120"
+        height="50"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+      <br/>
+      <br/>
+                <p class="p1">EL MEJOR LUGAR PARA REALIZAR TU EVENTO</p></Col>
 <Col><h5 >Contactos</h5>
-                    <li><a href="https://www.facebook.com/Hacienda-La-Escondida-754867987959305/about/?ref=page_internal">Facebook</a></li>
+                    
+                    <li><a href="https://wa.me/+5216142856669">614 285 6669</a></li>
+                    <li><a href="https://wa.me/+5216141318948">614 131 8948</a></li>
                     </Col>
-<Col><h5 >Informacion</h5>
+<Col>
+
+<h5 >Informacion</h5>
         
                     <li><a href="https://www.google.com/maps/dir/?api=1&destination=28.584703%2C-106.032236&fbclid=IwAR3YNE80RfhmvY5pxLkibCBBgNYKbrCQKh1poXZNjDhvTlZ5U4rLgbUMn7Q">Ubicacion</a></li>
-                    <li><a href="https://wa.me/+5216142856669">614 285 6669</a></li>
-                    <li><a href="https://wa.me/+5216141318948">614 131 8948</a></li></Col>
+                    <li><a href="https://www.facebook.com/Hacienda-La-Escondida-754867987959305/about/?ref=page_internal">Facebook</a></li></Col>
 </Row>
 </Container>
             
--- src/Components/ReactBootstrap/ReactBootstrap.js
+++ src/Components/ReactBootstrap/ReactBootstrap.js
@@ -1,13 +1,20 @@
 import React  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav} from 'react-bootstrap';
+import Logo from  './Logo.png';   
 
 export default function Bootstrap() {
   return(
 <>
 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="/home">La escondida</Navbar.Brand>
+  <Navbar.Brand href="/home"> <img
+        src={Logo}
+        width="120"
+        height="50"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      /></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
